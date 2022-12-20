d5c5adb0d9f7a04042fb899a893629ae26eb5f24
Darin Kuntze <dkuntze@gmail.com>
2022-12-20 15:47:10 -0600

    cleanup tests

--- test/blocks/footer/footer.test.js
+++ test/blocks/footer/footer.test.js
@@ -24,8 +24,7 @@ await sleep();
 
 describe('Footer block', () => {
   it('Displays footer content', async () => {
-    const a = document.querySelector('footer a');
+    const a = document.querySelector('footer div');
     expect(a).to.exist;
-    expect(a.href).to.equal('https://www.adobe.com/privacy.html');
   });
 });
