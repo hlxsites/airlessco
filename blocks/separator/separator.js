8009a5179d37477f0b6ce8557fd2b2fe42346977
vmitra <vmitra@adobe.com>
2022-12-21 13:47:47 -0500

    linting changes

--- blocks/separator/separator.js
+++ blocks/separator/separator.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-    const separator = document.createElement('hr');
-    separator.className = "separator-style";
-    block.append(separator);
-  }
+  const separator = document.createElement('hr');
+  separator.className = 'separator-style';
+  block.append(separator);
+}
