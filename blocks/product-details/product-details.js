85923f38ed0458c4d4e1633dd455c16e68b3b49b
Kunwar <kunwarsaluja@hotmail.com>
2022-12-23 09:30:53 -0500

    updating product-details

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -5,7 +5,7 @@ export default async function decorate(block) {
   const productName = [...block.children][1].innerText.trim('');
   const productFields = ['Series', 'Applications', 'Spray', 'Usage', 'Features', 'Includes', 'Availability', 'Resources', 'Images'];
 
-  // Make a call to the blog index and get the json for just the pathnames the author has put in
+  // Make a call to the  product datasheet  and get the json for all fields for the product
   const productData = await lookupProductData(productFamily, productName, productFields);
 
   const productDetailsDiv = createTag('div', { class: 'product-details-div' });
@@ -18,9 +18,20 @@ export default async function decorate(block) {
     if (productFields[index] === 'Images') {
       const productImage = createTag('img', { class: 'product-image' });
       productImage.setAttribute('src', item);
+      productImage.setAttribute('alt', 'product-image');
       productImageDiv.append(productImage);
       return;
     }
+
+    if (productFields[index] === 'Resources') {
+      label.innerText = productFields[index];
+      const itemText = createTag('div', { class: 'product-field' });
+      itemText.innerHTML = item;
+      row.append(label, itemText);
+      productDetails.append(row);
+      return;
+    }
+
     label.innerText = productFields[index];
     const itemText = createTag('div', { class: 'product-field' });
     itemText.innerText = item;
