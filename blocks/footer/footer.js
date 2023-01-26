e0b4ec37de41a859a9888d586c7951b046cf93c0
Darin Kuntze <dkuntze@gmail.com>
2023-01-26 12:00:09 -0600

    add mini-mobile footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,25 @@
 import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
 
+function createMobileMenu(footer) {
+  const mf = document.createElement('div');
+  mf.classList.add('air-footer-compressed');
+  const menuItems = footer.querySelectorAll('h3');
+  menuItems.forEach((menu, idx, arr) => {
+    mf.append(menu.firstChild);
+    if (idx !== (arr.length - 1)) {
+      mf.append(' | ');
+    }
+  });
+  // loop through and find the last div and inject our mobile footer
+  const footerdivs = footer.querySelectorAll('div');
+  footerdivs.forEach((fd, idx, arr) => {
+    if (idx === (arr.length - 1)) {
+      fd.insertAdjacentHTML('beforebegin', mf.outerHTML);
+      fd.classList.add('copyright');
+    }
+  });
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The header block element
@@ -16,6 +36,7 @@ export default async function decorate(block) {
     footer.classList.add('air-footer');
     footer.innerHTML = html;
     await decorateIcons(footer);
+    createMobileMenu(footer);
     block.append(footer);
   }
 }
