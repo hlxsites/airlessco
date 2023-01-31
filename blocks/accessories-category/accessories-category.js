6bbd577f95141b5705d17d8dfc2bca97b470f4e5
Bryan Stopp <bryan.stopp@gmail.com>
2023-01-31 12:40:03 -0600

    Finish mobile view of accessories list.

--- blocks/accessories-category/accessories-category.js
+++ blocks/accessories-category/accessories-category.js
@@ -1,52 +1,39 @@
-import { createTag, lookupProductData } from '../../scripts/scripts.js';
 import { createOptimizedPicture, fetchPlaceholders, getMetadata } from '../../scripts/lib-franklin.js';
+import { lookupProductData } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  const bc = {};
-  [...block.children].forEach((row) => {
-    if (row.querySelector('a')) {
-      bc.sheet = row.querySelector('a').href;
-    } else {
-      bc.product = row.textContent.trim();
-    }
-  });
+  const product = block.children[0].children[0].textContent.trim();
+  const sheet = block.querySelector('a').href;
 
-  const prefix = getMetadata('locale');
-  const placeholders = await fetchPlaceholders(prefix);
-
-  block.innerHTML = '';
-
-  const accList = createTag('ul');
-
-  const accessoriesInfo = await lookupProductData(bc.sheet, bc.product);
+  const placeholders = await fetchPlaceholders(getMetadata('locale'));
+  const accessoriesInfo = await lookupProductData(sheet, product);
   if (accessoriesInfo) {
-    const header = createTag('h2');
-    header.innerHTML = placeholders.moreaccessorieslabel || 'MORE ACCESSORIES';
-    block.append(header);
+    let html = `<h2><strong>${block.children[1].children[1].textContent.trim() || '<strong>MORE ACCESSORIES</strong>'}</strong></h2>`;
 
+    html += '<ul>';
     const accessories = accessoriesInfo.Accessories.split('\n');
     accessories.forEach((acc) => {
-      const accLink = placeholders[`${acc}Link`];
-      const accImage = placeholders[`${acc}Image`];
-      const accLabel = placeholders[`${acc}Label`];
-      if (accLink !== undefined || accImage !== undefined || accLabel !== undefined) {
-        const accItem = createTag('li');
-        accItem.innerHTML = `<a href="${accLink}">
-                <div class="productcards-productcard-div">  
-                    <div class="productcards-productcard-image">
-                        <picture>
-                            <img alt="${accLabel} image" src="${accImage}">
-                        </picture>
-                    </div>
-                    <div class="productcards-productcard-body">
-                        <p>${accLabel}</p>
-                    </div>
-                </div> 
-           </a>`;
-        accList.append(accItem);
-        accList.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '400' }])));
+      const link = placeholders[`${acc}Link`];
+      const image = placeholders[`${acc}Image`];
+      const label = placeholders[`${acc}Label`];
+      if (link && image && label) {
+        html += `
+            <li class="accessory">
+              <a href="${link}">
+                <div class="accessory-card">
+                  <div class="accessory-image">
+                    ${createOptimizedPicture(image, label, false, [{ width: '400' }]).outerHTML}
+                  </div>
+                  <div class="accessory-body">
+                      <p>${label}</p>
+                  </div>
+                </div>
+              </a>
+           </li>
+        `;
       }
     });
-    block.append(accList);
+    html += '</ul>';
+    block.innerHTML = html;
   }
 }
