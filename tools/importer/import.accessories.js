63c5fb889c5bfe521a67ceb94be33aae59be8eb1
Bryan Stopp <bryan.stopp@gmail.com>
2023-02-23 13:51:06 -0600

    Fixing import.

--- tools/importer/import.accessories.js
+++ tools/importer/import.accessories.js
@@ -15,7 +15,13 @@ import { createMetadataBlock } from './common.js';
 const createAutoBlock = (main, document) => {
   const title = document.createElement('h1');
   const h1 = document.querySelector('body > div.container.start > h1');
-  title.replaceChildren(...h1.childNodes);
+  let text = h1.innerHTML;
+  if (text.includes('<small>')) {
+    text = text.replace(/^(.*)<small>(.*)<\/small>$/, '<strong>$1</strong>$2');
+  } else {
+    text = text.replace(/^(.*)$/, '<strong>$1</strong>');
+  }
+  title.innerHTML = text;
   main.append(title);
   const body = document.querySelector('body > div.container.start > div.row.rowpadding > div');
   const p = document.createElement('p');
@@ -50,9 +56,18 @@ const createAccessoriesDetailBlock = (document, main, container, url) => {
     if (heading) {
       if (block) {
         main.append(WebImporter.DOMUtils.createTable(block, document));
+        main.append(document.createElement('hr'));
+        main.append(document.createElement('p'));
       }
       block = createDetailsBlockMarkdown(document);
-      block.push([heading.outerHTML]);
+      let title = heading.outerHTML;
+      if (title.includes('<small>')) {
+        title = title.replace(/^<h3>(.*)<small>(.*)<\/small><\/h3>$/, '<h3><strong>$1</strong>$2</h3>');
+      } else {
+        title = title.replace(/^<h3>(.*)<\/h3>$/, '<h3><strong>$1</strong></h3>');
+      }
+      block.push([title]);
+      productId = undefined;
     } else {
       if (row.querySelector('div.col-sm-4.col-md-3') && row.querySelector('div.col-sm-8.col-md-9')) {
         const secondCols = row.querySelectorAll('div.col-sm-8.col-md-9');
@@ -60,19 +75,20 @@ const createAccessoriesDetailBlock = (document, main, container, url) => {
 
         let cell = '';
         for (let i = 0; i < secondCols.length; i += 1) {
-          cell += `<p>${secondCols[i].textContent}</p>`;
+          cell += `<p>${secondCols[i].innerHTML.trim()}</p>`;
         }
         block.push([`<strong>${row.querySelector('div.col-sm-4.col-md-3').textContent}</strong>`, cell]);
       } else if (row.querySelector('div.col-sm-4.col-md-3') && row.querySelector('div.col-sm-12 table')) {
-        block.push([row.querySelector('div.col-sm-4.col-md-3').textContent, 'table']);
+        const tblLink = `https://main--airlessco--hlxsites.hlx.page/product-data/accessories/${productId.toLowerCase()}.json?sheet=${locale}`;
+        block.push([`<strong>${row.querySelector('div.col-sm-4.col-md-3').textContent}</strong>`, `<a href="${tblLink}">${tblLink}</a>`]);
       } else if (row.querySelector('div.col-sm-4.col-md-3')) {
-        block.push([row.querySelector('div.col-sm-4.col-md-3').textContent]);
+        block.push([`<strong>${row.querySelector('div.col-sm-4.col-md-3').textContent}</strong>`]);
       } else if (row.querySelector('div.col-xs-12 table')) {
         const tblLink = `https://main--airlessco--hlxsites.hlx.page/product-data/accessories/${productId.toLowerCase()}.json?sheet=${locale}`;
         block.push([`<a href="${tblLink}">${tblLink}</a>`]);
       } else if (row.querySelector('.table-responsive')) {
         const tblLink = `https://main--airlessco--hlxsites.hlx.page/product-data/accessories/${productId.toLowerCase()}.json?sheet=${locale}`;
-        block.push([`<strong>${row.querySelector('div.col-sm-12.col-md-3').textContent}</strong>`,`<a href="${tblLink}">${tblLink}</a>`]);
+        block.push([`<strong>${row.querySelector('div.col-sm-12.col-md-3').textContent}</strong>`, `<a href="${tblLink}">${tblLink}</a>`]);
       } else if (row.querySelector('div.col-xs-12')) {
         block.push([row.querySelector('div.col-xs-12').textContent]);
       }
@@ -108,24 +124,85 @@ const createMoreAccessoriesBlock = (main, document) => {
 
   const table = [
     ['Accessories Category'],
-    ['Title', `${heading.outerHTML}`],
+    ['Title', `<h2><strong>${heading.textContent}</strong></h2>`],
     ['Types', types.join('')],
   ];
   const tbl = WebImporter.DOMUtils.createTable(table, document);
   main.append(tbl);
 };
 
-const addMetadata = (main, document) => {
+const addMetadata = (main, document, url) => {
   const last = document.querySelector('ol.breadcrumb li.active');
   const meta = main.querySelector('table:last-of-type');
-  const tr = document.createElement('tr');
+  let tr = document.createElement('tr');
   let td = document.createElement('td');
   td.textContent = 'Nav Title';
   tr.append(td);
   td = document.createElement('td');
-  td.textContent = last.textContent.replace(/\s*Accessories\s*/g, '');
+  td.textContent = last.textContent.replace(/\s*(Accessories|Accesorios|Accessoires|Accessori|toebehoren|-toebehoren|Akcesoria)\s*/gi, '');
+  if (td.textContent === 'Příslušenství pro značení') {
+    td.textContent = 'Značení';
+  } else if (td.textContent === 'Zubehör Markiergeräte') {
+    td.textContent = 'Markierungsarbeiten';
+  } else if (td.textContent === 'Weiteres Zubehör') {
+    td.textContent = 'Sonstige';
+  } else if (td.textContent === 'HVLP-Zubehör') {
+    td.textContent = 'HVLP-ZERSTÄUBUNG';
+  } else if (td.textContent === 'de trazado') {
+    td.textContent = 'Trazado';
+  } else if (td.textContent === 'de traçage') {
+    td.textContent = 'Traçage';
+  } else if (td.textContent === 'per la tracciatura') {
+    td.textContent = 'Tracciatura';
+  } else if (td.textContent === 'Belijningstoebehoren') {
+    td.textContent = 'Belijning';
+  } else if (td.textContent === 'do malowania pasów') {
+    td.textContent = 'Malowania pasów';
+  }
+
   tr.append(td);
   meta.append(tr);
+
+  if (url.includes('fluids')) {
+    tr = document.createElement('tr');
+    td = document.createElement('td');
+    td.textContent = 'Image';
+    tr.append(td);
+    td = document.createElement('td');
+    const href = 'https://main--airlessco--hlxsites.hlx.live/media_1138b41fd9eb326a14a728cc32d8d0f3dc954c034.jpeg?width=2000&format=webply&optimize=medium';
+    const a = document.createElement('a');
+    a.setAttribute('href', href);
+    a.textContent = href;
+    td.append(a);
+    tr.append(td);
+    meta.append(tr);
+  } else if (url.includes('filters')) {
+    tr = document.createElement('tr');
+    td = document.createElement('td');
+    td.textContent = 'Image';
+    tr.append(td);
+    td = document.createElement('td');
+    const href = 'https://main--airlessco--hlxsites.hlx.live/media_181a18eab0d72d4d257aa01ddec4cdbe1f7496d0d.jpeg?width=2000&format=webply&optimize=medium';
+    const a = document.createElement('a');
+    a.setAttribute('href', href);
+    a.textContent = href;
+    td.append(a);
+    tr.append(td);
+    meta.append(tr);
+  } else if (url.includes('hoses')) {
+    tr = document.createElement('tr');
+    td = document.createElement('td');
+    td.textContent = 'Image';
+    tr.append(td);
+    td = document.createElement('td');
+    const href = 'https://main--airlessco--hlxsites.hlx.live/media_137a631ebcb314cb3628e5c0831e93aa42dbde0d2.jpeg?width=1200&format=pjpg&optimize=medium';
+    const a = document.createElement('a');
+    a.setAttribute('href', href);
+    a.textContent = href;
+    td.append(a);
+    tr.append(td);
+    meta.append(tr);
+  }
 };
 
 export default {
@@ -144,7 +221,7 @@ export default {
     createAccessorySections(main, document, url);
     createMoreAccessoriesBlock(main, document);
     createMetadataBlock(main, document, url);
-    addMetadata(main, document);
+    addMetadata(main, document, url);
     document.body.innerHTML = main.innerHTML;
     return document.body;
   },
