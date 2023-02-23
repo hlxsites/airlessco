389dc81a9b692df1555ae3528ea2110c53b6716b
Lamont Crook <lamont@adobe.com>
2023-02-22 20:04:43 -0700

    add google tag manager support to delayed.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -181,7 +181,6 @@ async function loadLazy(doc) {
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
-
 }
 
 /**
